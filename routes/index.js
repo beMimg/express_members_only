5661d16f53289273c801ad7fc5ec79cd863fff20
beMimg <bernardommguerreiro@gmail.com>
2024-03-03 11:42:04 +0100

    some styling index

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,19 @@ const mongoose = require("mongoose");
 const Message = require("../models/message");
 /* GET home page. */
 router.get("/", async (req, res, next) => {
-  try {
-    const messages = await Message.find().exec();
-    res.render("index", { title: "Express", user: req.user });
-  } catch (err) {
-    return next(err);
+  if (req.isUnauthenticated()) {
+    try {
+      const messages = await Message.find().exec();
+      res.render("index", {
+        title: "Express",
+        user: req.user,
+        messages: messages,
+      });
+    } catch (err) {
+      return next(err);
+    }
+  } else {
+    res.redirect("/dashboard");
   }
 });
 
